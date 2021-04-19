486d6fdd285a3398f221b72c06beb45a2fa9e57a
Jose Eduar <JoseEduardoGarciaAvalos@users.noreply.github.com>
2021-04-19 14:41:36 -0600

    fix reference of deleted component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { ResultadoComponent } from "./view/resultado/resultado";
 import { QuizComponent } from "./view/quiz/quiz";
 import { MainComponent } from "./view/main/main";
 
 const routes: Routes = [
   { path: "", component: MainComponent },
-  { path: "quiz/:code", component: QuizComponent },
-  { path: "resultado", component: ResultadoComponent }
+  { path: "quiz/:code", component: QuizComponent }
 ];
 
 @NgModule({
